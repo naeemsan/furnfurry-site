ddc6ef8c7a5b078face16f6314144f75f219a6a8
Naeem San <naeemsan221@gmail.com>
2026-05-14 06:23:57 +0500

    finally

--- worker.js
+++ worker.js
@@ -1,5 +1,41 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+
+    try {
+      if (request.method === "HEAD") {
+        const getRequest = new Request(request.url, {
+          method: "GET",
+          headers: request.headers,
+        });
+
+        const response = await env.ASSETS.fetch(getRequest);
+
+        return new Response(null, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: response.headers,
+        });
+      }
+
+      return await env.ASSETS.fetch(request);
+    } catch (error) {
+      const fallbackRequest = new Request(url.origin + "/", {
+        method: request.method === "HEAD" ? "GET" : request.method,
+        headers: request.headers,
+      });
+
+      const response = await env.ASSETS.fetch(fallbackRequest);
+
+      if (request.method === "HEAD") {
+        return new Response(null, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: response.headers,
+        });
+      }
+
+      return response;
+    }
   },
 };
