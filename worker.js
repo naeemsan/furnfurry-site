77eb27097a36f6c3e7b6b91196ed3dd30cda2f73
Naeem San <naeemsan221@gmail.com>
2026-05-14 06:52:07 +0500

    hogaya

--- worker.js
+++ worker.js
@@ -1,30 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-
-    if (request.method === "HEAD") {
-      return new Response(null, {
-        status: 200,
-        headers: {
-          "Content-Type": "text/html; charset=UTF-8",
-          "Cache-Control": "public, max-age=300",
-        },
-      });
-    }
-
-    if (request.method === "GET") {
-      try {
-        return await env.ASSETS.fetch(request);
-      } catch {
-        return await env.ASSETS.fetch(
-          new Request(url.origin + "/", {
-            method: "GET",
-            headers: request.headers,
-          })
-        );
-      }
-    }
-
-    return await env.ASSETS.fetch(request);
+    return env.ASSETS.fetch(request);
   },
 };
